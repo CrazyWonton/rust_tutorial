b4223b293ce678e876f24a3e2656d0e7df50590d
Crazywonton <kevintarc13@gmail.com>
2021-03-07 10:37:28 -0600

    Print line and formatting module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod print;
+
 fn main() {
-    println!("Hello, world!");
+	// Printing 
+	print::run();
+
+	// Formatting
+	println!("{} have {} cats", "Do I", 413);
+
+	// Positional Arguments
+	println!("{0} is a {1}. {0} is also a {2}", "Sydney", "dog", "city");
+
+	// Named Arguments
+	println!("{player} is level {number}", player="Drenor", number="4"); 
+
+	// Placeholder Traits
+	println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
+
+	// Placeholder for debug trait
+	println!("{:?}", (12, true, "Hello"));
+
+	// Basic math
+	println!("10 + 10 = {}", 10+10);
 }
